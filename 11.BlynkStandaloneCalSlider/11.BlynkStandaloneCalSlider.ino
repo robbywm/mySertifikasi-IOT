3ce805c96bfc173a7f07b801c722c0ed105380c3
Robby Wildan Muharam <115667651+robbywm@users.noreply.github.com>
2025-12-09 15:54:06 +0700

    Edit keseluruhan

--- 11.BlynkStandaloneCalSlider/11.BlynkStandaloneCalSlider.ino
+++ 11.BlynkStandaloneCalSlider/11.BlynkStandaloneCalSlider.ino
@@ -1,31 +1,3 @@
-/*************************************************************
-  Blynk is a platform with iOS and Android apps to control
-  ESP32, Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build mobile and web interfaces for any
-  projects by simply dragging and dropping widgets.
-
-    Downloads, docs, tutorials: https://www.blynk.io
-    Sketch generator:           https://examples.blynk.cc
-    Blynk community:            https://community.blynk.cc
-    Follow us:                  https://www.fb.com/blynkapp
-                                https://twitter.com/blynk_app
-
-  Blynk library is licensed under MIT license
-  This example code is in public domain.
-
- *************************************************************
-  This example runs directly on ESP8266 chip.
-
-  NOTE: This requires ESP8266 support package:
-    https://github.com/esp8266/Arduino
-
-  Please be sure to select the right ESP8266 module
-  in the Tools -> Board menu!
-
-  Change WiFi ssid, pass, and Blynk auth token to run :)
-  Feel free to apply it to any other example. It's simple!
- *************************************************************/
-
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
 
@@ -34,14 +6,32 @@
 #define BLYNK_TEMPLATE_NAME "SESUAIKAN"
 #define BLYNK_AUTH_TOKEN "SESUAIKAN"
 
+// Pin LED dan buzzer
+#define ledBiru D13
+#define ledMerah D12
+#define ledR D9
+#define ledB D10
+#define ledG D11
+#define buzzerPin D5
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+#define DHTPIN D4    
+#define DHTTYPE DHT11  
+#define pot A0
+DHT dht(DHTPIN, DHTTYPE);
+
+// Timer
+BlynkTimer timer;
+
+// Nilai kalibrasi dari slider V8
+int calibrationSliderValue = 0;
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "RR2";
-char pass[] = "kivandra";
+char ssid[] = "SESUAIKAN";
+char pass[] = "SESUAIKAN";
 
 void setup()
 {
@@ -49,10 +39,115 @@ void setup()
   Serial.begin(9600);
 
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+    // Set pin output
+  pinMode(ledBiru, OUTPUT);
+  pinMode(ledMerah, OUTPUT);
+  pinMode(ledR, OUTPUT);
+  pinMode(ledG, OUTPUT);
+  pinMode(ledB, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+  dht.begin();
+
+  // Set interval pembacaan sensor
+  timer.setInterval(5000L, sensor_dht11);
+  timer.setInterval(1000L, analogInput);
 }
 
 void loop()
 {
   Blynk.run();
+  timer.run();
+}
+
+// Kontrol LED dari aplikasi Blynk
+BLYNK_WRITE(V0)
+{
+  int pinValue = param.asInt();
+  Serial.print("V0 Switch value is: ");
+  Serial.println(pinValue);
+  digitalWrite(ledBiru, pinValue);
+}
+
+BLYNK_WRITE(V1)
+{
+  int pinValue = param.asInt();
+  Serial.print("V1 Switch value is: ");
+  Serial.println(pinValue);
+  digitalWrite(ledMerah, pinValue);
+}
+
+BLYNK_WRITE(V2)
+{
+  int pinValue = param.asInt();
+  Serial.print("V2 Switch value is: ");
+  Serial.println(pinValue);
+  digitalWrite(ledR, pinValue);
+}
+
+BLYNK_WRITE(V3)
+{
+  int pinValue = param.asInt();
+  Serial.print("V3 Switch value is: ");
+  Serial.println(pinValue);
+  digitalWrite(ledG, pinValue);
+}
+
+BLYNK_WRITE(V4)
+{
+  int pinValue = param.asInt();
+  Serial.print("V4 Switch value is: ");
+  Serial.println(pinValue);
+  digitalWrite(ledB, pinValue);
+}
+
+// Fungsi pembacaan sensor DHT11
+void sensor_dht11() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // Kalibrasi suhu dengan nilai dari slider
+  t += calibrationSliderValue;
+
+  // Tampilkan di Serial Monitor
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C\t");
+
+  // Kirim ke Blynk
+  Blynk.virtualWrite(V5, t);
+  Blynk.virtualWrite(V6, h);
+
+  // Jika suhu tinggi, aktifkan buzzer
+  if (t >= 32) {
+    for (int i = 0; i < 4; i++) { 
+      tone(buzzerPin, 1000, 200); // bunyi 1kHz selama 200ms
+      delay(400);  
+    }
+    Serial.print("Status: ");
+    Serial.println("Melebihi Batas");
+  } else {
+    Serial.print("Status: ");
+    Serial.println("Aman");  
+  }
+}
+
+void analogInput(){
+  int potValue = analogRead(pot);
+  Blynk.virtualWrite(V7,potValue);
+  //if(potValue >= 900){
+  //tone(buzzerPin, 1000, 500); 
+  //}
+}
+
+// Slider untuk kalibrasi suhu (Virtual Pin V8)
+BLYNK_WRITE(V8)
+{
+  calibrationSliderValue = param.asInt(); // nilai dari -10 hingga +10
+  Serial.print("Calibration value from V8: ");
+  Serial.println(calibrationSliderValue);
 }
 
